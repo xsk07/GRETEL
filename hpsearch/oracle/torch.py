Data normalization and fixed hyperparameters optimizer

hpsearch/oracle/torch.py
@@ -33,9 +33,9 @@ class OracleTorch(TorchBase, Oracle):
             preds += list(pred.squeeze().detach().to('cpu').numpy())
          
         accuracy = self.accuracy(labels_list, preds)
-        self.context.logger.info(f'Test accuracy = {np.mean(accuracy):.4f}')
-        return np.mean(accuracy)
-
+        self.mean_accuracy = np.mean(accuracy)
+        self.context.logger.info(f'Test accuracy = {self.mean_accuracy:.4f}')
+        
 
     def _real_predict(self, data_instance):
         return torch.argmax(self._real_predict_proba(data_instance)).item()

src/dataset/generators/dblp_v1.py
@@ -1,5 +1,4 @@
 from os.path import join
-from random import shuffle
 import numpy as np
 
 from src.dataset.generators.base import Generator
@@ -12,10 +11,8 @@ class DBLP(Generator):
         #configuration parameters
         base_path = self.local_config['parameters']['data_dir']
         self.dataset_name = self.local_config['parameters']['dataset_name']
-        #self.min_nodes = self.local_config['parameters']['min_nodes']
-        #self.max_nodes = self.local_config['parameters']['max_nodes']
 
-        # Paths to the files of the DBLP_v1 dataset
+        #Paths to the files of the DBLP_v1 dataset
 
         #sparse (block diagonal) adjacency matrix for all graphs (node_id, node_id)
         self._adj_file_path = join(base_path, f"{self.dataset_name}_A.txt")
@@ -29,7 +26,7 @@ class DBLP(Generator):
         #column vector of node labels
         self._nl_file_path = join(base_path, f"{self.dataset_name}_node_labels.txt")
 
-        #labels for the edges in DS_A_sparse.txt 
+        #labels for the edges in DS_A_sparse.txt
         self._el_file_path = join(base_path, f"{self.dataset_name}_edge_labels.txt")
 
         self.dataset.node_features_map = {'node_feature': 0}
@@ -40,8 +37,8 @@ class DBLP(Generator):
     def generate_dataset(self):
         
         if not self.get_num_instances():
-            
-            #load the dataset
+
+            #Loading the dataset
             labels = np.loadtxt(self._gl_file_path, dtype=int)
             node_labels = np.loadtxt(self._nl_file_path, dtype=int)
             edges = np.genfromtxt(self._adj_file_path, dtype=int, delimiter=',')
@@ -57,9 +54,14 @@ class DBLP(Generator):
             #list of edges in which each node's number is replaced with the graph id it belongs to
             edges_gid = graph_ind[edges]
 
+            #min-max normalization
+            min_val = np.min(node_labels)
+            max_val = np.max(node_labels)
+            node_labels = (node_labels-min_val)/(max_val-min_val)
+
             graph_ids = np.unique(graph_ind) #list of graph identifiers
-            np.random.shuffle(graph_ids)
-            for id in graph_ids: #[:1000]:
+            #np.random.shuffle(graph_ids)#shuffle randomly the graph ids
+            for id in graph_ids:
 
                 #masks for filtering accordingly to the graph identifier
                 node_mask = (graph_ind == id) #mask for nodes
@@ -78,24 +80,12 @@ class DBLP(Generator):
                 assert np.all(np.diff(np.unique(mapped_edges))==1)
 
                 adj_matrix = self.create_adj_matrix(mapped_edges)
-                # manages singluar matrixes,
-                # we add a small constant to the diagonal so that the determinant is non-zero anymore
-                # if np.isclose(np.linalg.det(adj_matrix), 0):
-                #adj_matrix += 1e-10 * np.eye(adj_matrix.shape[0])
-                # ensures that the matrix is non-singular
-                # assert not np.isclose(np.linalg.det(adj_matrix), 0), f"singular matrix found!"
 
                 if not np.isclose(np.linalg.det(adj_matrix), 0):
-                #if True:
 
-                    # normalization of the node feature
                     node_features = node_labels[node_mask]
+                    node_features = node_features[:, np.newaxis]
 
-                    #min-max normalization
-                    min_val = np.min(node_features)
-                    max_val = np.max(node_features)
-                    node_features = (node_features-min_val)/(max_val-min_val)
-                    node_features=node_features[:, np.newaxis]
                     # one-hot encoding of the edge lables
                     edge_features = np.eye(3)[edge_labels[np.any(edge_mask, axis=1)]]
                     
@@ -109,11 +99,9 @@ class DBLP(Generator):
                         )
                     )
 
-
-
     def create_adj_matrix(self, edges):
         # after the mapping, nodes are numbered from 0 to max
-        num_nodes = np.max(edges)+1 
+        num_nodes = np.max(edges)+1
         adj_matrix = np.zeros((num_nodes, num_nodes), dtype=int)
         # The edge list takes into account that the edges are undirected,
         # so already it contains the edges of the two possible permutations of nodes,
@@ -121,6 +109,6 @@ class DBLP(Generator):
         adj_matrix[edges[:,0], edges[:,1]] = 1
         
         #adj_matrix[edges[:,1], edges[:,0]] = 1
-        return adj_matrix    
+        return adj_matrix
     
     